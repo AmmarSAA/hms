636881720b05c6fdd816183e293d05617e8341ed
Ammar S.A.A <s.ammarahmed14@gmail.com>
2022-04-11 22:27:22 +0500

    Initial Commit

---
+++ footer.php
@@ -0,0 +1,77 @@
+    <footer class="footer-2">
+    <div class="container">
+      <div class="row"> 
+        
+        <!-- Support -->
+        <div class="col-sm-7">
+          <h6>About Us</h6>
+          <div class="about-foot">
+            <p>A concern of Care and Cure Group is the only multi-disciplinary super-specialty tertiary care hospital in Karachi, Pakistan, confidently providing comprehensive health care with the latest medical, surgical and diagnostic facilities. These services are provided by expert medical professionals, skilled nurses and technologists using state-of-the-art technology. </p>
+          </div>
+        </div>
+        
+       
+        
+        <!-- Conatct -->
+        <div class="col-sm-5">
+          <div class="con-info">
+            <table class="table table-sm">
+              <thead>
+                <tr>
+                  <th><h6>Contact Us</h6></th>
+                  <th><h6>SOCIAL LINKS</h6></th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td><p>1942 Poe Lane, Kansas City.</p></td>
+                  <td><p><a href="#">Facebook</a></p></td>
+                </tr>
+                <tr>
+                  <td class="mobile"><p>1010 2020 36360</p></td>
+                  <td><p><a href="#">Youtube</a></p></td>
+                </tr>
+                <tr>
+                  <td class="mobile"><p>9-269-690-HMS</p></td>
+                  <td><p><a href="#">Twitter</a></p></td>
+                </tr>
+                <tr>
+                  <td class="email"><p>info@hmsphp.com</p></td>
+                  <td><p><a href="#">LinkedIn</a></p></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  
+  <!-- Rights -->
+  <div class="rights style-2">
+    <div class="container">
+      <p>© <?php echo date("Y"); ?> Hospital Management System | Developed By: <strong><span></span><a href="https://www.github.com/ammarsaa" style="color: white">Ammar S.A.A</a></span></strong>
+    </div>
+  </div>
+  
+  <!-- GO TO TOP  --> 
+  <a href="#" class="cd-top"><i class="fa fa-angle-up"></i></a> 
+  <!-- GO TO TOP  --> 
+</div>
+<!-- End Page Wrapper --> 
+
+<!-- JavaScripts --> 
+<script src="js/vendors/jquery/jquery.min.js"></script> 
+<script src="js/vendors/wow.min.js"></script> 
+<script src="js/vendors/bootstrap.min.js"></script> 
+<script src="js/vendors/own-menu.js"></script> 
+<script src="js/vendors/jquery.sticky.js"></script> 
+<script src="js/vendors/owl.carousel.min.js"></script> 
+<script src="js/vendors/jquery.magnific-popup.min.js"></script> 
+
+<!-- SLIDER REVOLUTION 4.x SCRIPTS  --> 
+<script type="text/javascript" src="rs-plugin/js/jquery.tp.t.min.js"></script> 
+<script type="text/javascript" src="rs-plugin/js/jquery.tp.min.js"></script> 
+<script src="js/main.js"></script>
+</body>
+</html>
---
+++ viewadmin.php
@@ -0,0 +1,59 @@
+<?php
+include("adformheader.php");
+include("dbconnection.php");
+if(isset($_GET[delid]))
+{
+	$sql ="DELETE FROM admin WHERE adminid='$_GET[delid]'";
+	$qsql=mysqli_query($con,$sql);
+	if(mysqli_affected_rows($con) == 1)
+	{
+		echo "<script>alert('admin record deleted successfully..');</script>";
+	}
+}
+?>
+
+<div class="container-fluid">
+  <div class="block-header">
+    <h2>View Adminstrators</h2>
+  </div>
+</div>
+<div class="card">
+  <section class="container">
+   <table class="table table-bordered table-striped table-hover js-basic-example dataTable">
+
+
+    <thead>
+      <tr>
+        <td width="12%" height="40"><strong>Admin Name</strong></td>
+        <td width="11%"><strong>Login ID</strong></td>
+        <td width="12%"><strong>Status</strong></td>
+        <td width="10%"><strong>Action</strong></td>
+      </tr>
+    </thead>
+    <tbody>
+     <?php
+     $sql ="SELECT * FROM admin";
+     $qsql = mysqli_query($con,$sql);
+     while($rs = mysqli_fetch_array($qsql))
+     {
+      echo "<tr>
+      <td>$rs[adminname]</td>
+      <td>$rs[loginid]</td>
+      <td>$rs[status]</td>
+      <td>
+      <a href='admin.php?editid=$rs[adminid]' class='btn btn-sm btn-raised g-bg-cyan'>Edit</a> <a href='viewadmin.php?delid=$rs[adminid]' class='btn btn-sm btn-raised g-bg-blush2'>Delete</a> </td>
+      </tr>";
+    }
+    ?>
+  </tbody>
+</table>
+</section>
+
+</div>
+</div>
+
+
+
+<?php
+include("adformfooter.php");
+?>
